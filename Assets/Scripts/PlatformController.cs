7635efc8caab7a7a9a3f47353e2c5c9103e2f82f
ata kaleli <atakaleli123@gmail.com>
2024-09-30 00:03:05 +0300

    Platform coloring is added

--- Assets/Scripts/PlatformController.cs
+++ Assets/Scripts/PlatformController.cs
@@ -4,7 +4,26 @@ using UnityEngine;
 
 public class PlatformController : MonoBehaviour
 {
-    
+    [SerializeField] private SpriteRenderer sr;
+    [SerializeField] private SpriteRenderer headerSr;
+    [SerializeField] private Transform platformHeader;
+
+
+    private void Start()
+    {
+        platformHeader.parent = transform.parent;
+        headerSr.transform.localScale = new Vector2(sr.bounds.size.x, 0.2f);
+        headerSr.transform.position = new Vector2(sr.transform.position.x, sr.bounds.max.y);
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.GetComponent<Player>() != null)
+        {
+            headerSr.color = GameManager.instance.platformColor;
+        }
+    }
 
 
 
