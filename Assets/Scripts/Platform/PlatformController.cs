f731048ca8e872d3b7755574e381e52cb5bc629e
ata kaleli <atakaleli123@gmail.com>
2024-10-17 10:40:08 +0300

    Platform head coloring bug is fixed
    Platform collider is moved to header collider

--- Assets/Scripts/Platform/PlatformController.cs
+++ Assets/Scripts/Platform/PlatformController.cs
@@ -4,20 +4,30 @@ using UnityEngine;
 
 public class PlatformController : MonoBehaviour
 {
+
+
     [SerializeField] private SpriteRenderer sr;
     [SerializeField] private SpriteRenderer headerSr;
     [SerializeField] private Transform platformHeader;
 
+
     [SerializeField] private bool isFirstPlatform;
 
 
+
+    
     private void Start()
     {
-        platformHeader.parent = transform.parent;
+
+        platformHeader.parent = sr.transform.parent;
         headerSr.transform.localScale = new Vector2(sr.bounds.size.x, 0.15f);
         headerSr.transform.position = new Vector2(sr.transform.position.x, sr.bounds.max.y);
+
+        
     }
 
+
+    
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
